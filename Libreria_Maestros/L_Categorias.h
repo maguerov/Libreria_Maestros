63957fb8703898c5fc1acb5f99055bb26e1a13d7
zebazttian <51104612+zebazttian@users.noreply.github.com>
2021-04-16 16:44:08 -0600

    actualizacion categoria.h
    incompeto

--- Libreria_Maestros/L_Categorias.h
+++ Libreria_Maestros/L_Categorias.h
@@ -1,5 +1,20 @@
 #pragma once
+#include "NodoCategoria.h"
+
 class L_Categorias
 {
+
+private:
+	NodoCategoria* cab;
+	int largo;
+public:
+
+	L_Categorias();
+	int getLargo();
+	void setLargo(int largo);
+	NodoCategoria* getCab();
+	void setCab(NodoCategoria* cab);
+	void desplegar();
+	bool esVacia();
 };
 
